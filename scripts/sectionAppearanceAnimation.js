b9d6311fe7a4f87ced4e4f0abb0c0863c4dc9488
incora-dev <dev@incorainc.com>
2025-08-05 12:01:18 +0300

    [upd]: added section appearance animation

---
+++ scripts/sectionAppearanceAnimation.js
@@ -0,0 +1,21 @@
+function sectionsAnimation() {
+  const sections = document.querySelectorAll(
+    "section:has([class$='__container'])"
+  );
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.5 }
+  );
+
+  sections.forEach((section) => observer.observe(section));
+}
+
+sectionsAnimation();
